91a31739bed1bf74b924f26f9b9fc4be105b8be9
Vidit Shah <vshah4@sapient.com>
2017-03-08 11:33:40 +0530

    *added form validator component to forms module

--- src/app/views/forms/form-validator/form-validator.component.ts
+++ src/app/views/forms/form-validator/form-validator.component.ts
@@ -1,15 +1,36 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { FileReadService } from '../../../core/services/file-read.service';
 
 @Component({
   selector: 'app-form-validator',
   templateUrl: './form-validator.component.html',
   styleUrls: ['./form-validator.component.scss']
 })
-export class FormValidatorComponent implements OnInit {
 
-  constructor() { }
+export class FormValidatorComponent implements OnInit {
+  // start
+  detailsForm: FormGroup;
+
+  constructor(private _fileReadService: FileReadService, private _formBuilder: FormBuilder) { 
+    this.detailsForm = this._formBuilder.group({
+      firstName: [null, [Validators.required, Validators.minLength(3)]],
+      lastName: [null, [Validators.required, Validators.minLength(3)]],
+      isIndian: [null, [Validators.required]],
+      primaryLanguage: [null, [Validators.required]]
+    });
+  }
+  // end
+
+  tsFileContent: string;
+  htmlFileContent: string;
 
   ngOnInit() {
+    this._fileReadService.getFileContents('src/app/views/forms/form-validator/form-validator.component.ts')
+      .subscribe(tsFileContent => this.tsFileContent = this._fileReadService
+        .trimContent(tsFileContent, FileReadService.TS));
+    this._fileReadService.getFileContents('src/app/views/forms/form-validator/form-validator.component.html')
+      .subscribe(htmlFileContent => this.htmlFileContent = this._fileReadService
+        .trimContent(htmlFileContent, FileReadService.HTML));
   }
-
 }
